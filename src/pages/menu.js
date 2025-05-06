1a48964d5e2bb92b813160c1e787dadc5334e5af
demi <demial003@gmail.com>
2025-05-06 15:17:33 -0400

    create menu items

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,8 +1,20 @@
 const render = () => {
   const container = document.getElementById("content");
+
   const menu = document.createElement("div");
   menu.classList.add("menu");
   container.appendChild(menu);
+
+  const createMenuItem = (name) => {
+    const item = document.createElement("div");
+    item.classList.add("item");
+    item.classList.add(name);
+    menu.appendChild(item);
+  };
+
+  for (let i = 0; i < 9; i++) {
+    createMenuItem("pizza");
+  }
 };
 
 export default render;
